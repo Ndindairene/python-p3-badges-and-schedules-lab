00e140141d2764733da98e04d9bef1a7d9d58918
irene <irene.ndindaa@gmail.com>
2024-01-18 16:48:24 +0300

    completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
-def badge_maker(name):
-    return None
+def badge_maker(name = 'irene'):
+    return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    return [f'Hello, my name is {name}.' for name in names]
+
+speakers = ["Arel", "Carol"]
+badge_messages = batch_badge_creator(speakers)
+print(badge_messages)
+
 
 def assign_rooms(names):
-    return None
+    return([f'Hello, {name}! You\'ll be assigned to room {i + 1}!' for i, name in enumerate(names)])
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for assignment in room_assignments:
+        print(assignment)
+
+
